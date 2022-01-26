56ded992f60b07762bfec7e688035db219695dc0
r-e-d-ant <mugishathierry65@gmail.com>
2022-01-26 13:11:49 +0200

    added prevent default refleshing on form submit:

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
 
+/* --- setting modal toggling settings --- */
 const settingIcons = document.querySelectorAll('.setting-icon-container');
 const settingModals = document.querySelectorAll('.setting-modal-container');
 
 settingIcons.forEach(settingIcon => {
     settingIcon.addEventListener('click', () => {
         settingIcon.parentElement.previousElementSibling.classList.toggle('show-setting-modal')
+    })
+})
+
+/* ------------------------------------- */
+
+const submitPreventDefaultBtns = document.querySelectorAll('.submit-prevent-default')
+
+submitPreventDefaultBtns.forEach(submitPreventDefaultBtn => {
+    submitPreventDefaultBtn.addEventListener("click", (e) => {
+        e.preventDefault()
     })
 })
